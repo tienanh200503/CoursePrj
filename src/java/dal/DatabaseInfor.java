b0dc5d963f4a01b038e09efd0c3528cc14cd7fdc
leetienks1 <phamanhvs20@gmail.com>
2024-03-02 20:42:40 +0700

    update

---
+++ src/java/dal/DatabaseInfor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dal;
+
+/**
+ *
+ * @author BIN
+ */
+public interface DatabaseInfor {
+    String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=Course;";
+    String user = "sa";
+    String pass = "123";
+}
---
+++ src/java/model/Course.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author BIN
+ */
+public class Course {
+    private String course_name;
+    private String course_time;
+    private String course_price;
+    private int teacher_id;
+
+    public Course() {
+    }
+
+    public Course( String course_name, String course_time, String course_price, int teacher_id) {
+        this.course_name = course_name;
+        this.course_time = course_time;
+        this.course_price = course_price;
+        this.teacher_id = teacher_id;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_time() {
+        return course_time;
+    }
+
+    public void setCourse_time(String course_time) {
+        this.course_time = course_time;
+    }
+
+    public String getCourse_price() {
+        return course_price;
+    }
+
+    public void setCourse_price(String course_price) {
+        this.course_price = course_price;
+    }
+
+    public int getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(int teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+    
+    
+    
+    
+}
